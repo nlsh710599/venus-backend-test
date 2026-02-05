077a575c5da4251c4cb8364edb32a70357ad99de
cyliu1018 <chihyen.liu@ambrus.studio>
2026-02-05 18:22:20 +0800

    test: configure jest and exclude e2e tests from default runner

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,11 +1,12 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
     ...tsJestTransformCfg,
   },
+  testPathIgnorePatterns: ['/node_modules/', '\\.e2e\\.test\\.ts$'],
 };
